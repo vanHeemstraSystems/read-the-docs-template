145fdace64aa05f3782a1d193b51b95a2260397a
wvanheemstra <wvanheemstra@users.noreply.github.com>
2024-11-01 14:06:02 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PROJECT_NAME'
+copyright = '2024 - present, van Heemstra Systems'
+author = 'Willem van Heemstra'
 
 release = '0.1'
 version = '0.1.0'
